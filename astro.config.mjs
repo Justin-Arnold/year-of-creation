27bedb65ef9ad5abd1e7f0ceb3a7c80ad7755834
Justin Arnold <hello@justin-arnold.com>
2025-08-10 00:20:19 -0400

    feat: Convert to new tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind()]
+  integrations: [vue()],
+  vite: {
+        plugins: [
+            tailwindcss()
+        ]
+    },
 });
